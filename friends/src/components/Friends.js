d423c4968cf05cf06fef7c679865b8c85d45a7ad
borjasolerr <borjasolrey@gmail.com>
2019-02-27 16:35:24 +0100

    Added friends component

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Friends extends React.Component {
+  state = {
+    friends: null,
+    error: null,
+    loading: false
+  };
+
+  componentDidMount() {
+    this.fetchFriends();
+  }
+
+  fetchFriends = () => {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => this.addFriends(res.data))
+      .catch(res => this.addError(res.message));
+  };
+  addFriends = friends => {
+    this.setState({ friends: friends });
+  };
+
+  addError = error => {
+    this.setState({ error: error });
+  };
+
+  render() {
+    console.log(this.state.friends);
+    return (
+      <div>
+        <div>
+          {this.state.friends &&
+            this.state.friends.map(friend => {
+              return (
+                <div key={friend.name}>
+                  <div>Name: {friend.name}</div>
+                  <div>Age: {friend.age}</div>
+                  <div>Email: {friend.email}</div>
+                </div>
+              );
+            })}
+          {!this.state.friends && <div>Loading friends...</div>}
+        </div>
+      </div>
+    );
+  }
+}
